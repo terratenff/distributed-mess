228d2b3e43ee5de96cf2fbd7f26d8141f8e98de4
terratenff <49789537+terratenff@users.noreply.github.com>
2022-12-16 11:16:39 +0200

    Minor adjustments, and disable React scripts for now

--- surface/src/main/java/org/tt/field/controllers/front/GeneralController.java
+++ surface/src/main/java/org/tt/field/controllers/front/GeneralController.java
@@ -12,12 +12,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(value = "")
 public class GeneralController {
+
     private static Logger logger = LoggerFactory.getLogger(GeneralController.class);
 
-    @GetMapping("")
-    public ModelAndView index(final HttpServletRequest request) {
-        logger.info("INDEX");
+    private final String HOME = "";
+    private final String MISSION_CONTROL = "/mission_control";
+    private final String RESOURCES = "/resources";
+    private final String INBOUND_SHIPS = "/inbound_ships";
+    private final String OUTBOUND_SHIPS = "/outbound_ships";
+    private final String ACTIVE_SHIPS = "/active_ships";
 
+    @GetMapping(HOME)
+    public ModelAndView index(final HttpServletRequest request) {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("base");
         mav.addObject("pageContext", "index");
@@ -25,13 +31,47 @@ public class GeneralController {
         return mav;
     }
 
-    @RequestMapping("/none")
-    public ModelAndView base(final HttpServletRequest request) {
-        logger.info("BASE");
-
+    @GetMapping(MISSION_CONTROL)
+    public ModelAndView missionControl(final HttpServletRequest request) {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("base");
-        mav.addObject("pageContext", "none");
+        mav.addObject("pageContext", "mission_control");
+
+        return mav;
+    }
+
+    @GetMapping(RESOURCES)
+    public ModelAndView resources(final HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("base");
+        mav.addObject("pageContext", "resources");
+
+        return mav;
+    }
+
+    @GetMapping(INBOUND_SHIPS)
+    public ModelAndView inboundShips(final HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("base");
+        mav.addObject("pageContext", "inbound_ships");
+
+        return mav;
+    }
+
+    @GetMapping(OUTBOUND_SHIPS)
+    public ModelAndView outboundShips(final HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("base");
+        mav.addObject("pageContext", "outbound_ships");
+
+        return mav;
+    }
+
+    @GetMapping(ACTIVE_SHIPS)
+    public ModelAndView activeShips(final HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("base");
+        mav.addObject("pageContext", "active_ships");
 
         return mav;
     }
