04f6991f3ce106bb25e038b81cda6f1a2c3db55d
terratenff <49789537+terratenff@users.noreply.github.com>
2023-06-08 06:35:47 +0300

    Limited number of logs and events on home page and added means of showing more

---
+++ surface/frontend/src/AppFooter.js
@@ -0,0 +1,9 @@
+function AppFooter() {
+    return (
+        <div style={{backgroundColor: "#282c34", width: "100%", height: "400px"}}>
+
+        </div>
+    );
+}
+
+export default AppFooter;
--- surface/frontend/src/Home.js
+++ surface/frontend/src/Home.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Container, Table, Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+import { Container, Button, Table, Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
 import { motion } from 'framer-motion';
 import './App.css';
 import AppNavbar from './AppNavbar';
+import AppFooter from './AppFooter';
 import spaceship from "./images/spaceship.png";
 import HomeCarousel from "./components/HomeCarousel";
 
@@ -18,9 +19,26 @@ function Home() {
         setMissions(fetchedMissions);
     }
 
+    async function refresh() {
+        fetchShips();
+        fetchMissions();
+    }
+
+    async function showMoreShipLogs() {
+        setMaxLogs(maxLogs + loadIncrement);
+    }
+
+    async function showMoreMissionEvents() {
+        setMaxEvents(maxEvents + loadIncrement);
+    }
+
+    const loadIncrement = 12;
+
     const [ships, setShips] = useState([]);
     const [missions, setMissions] = useState([]);
 
+    const [maxLogs, setMaxLogs] = useState(loadIncrement);
+    const [maxEvents, setMaxEvents] = useState(loadIncrement);
     const initialized = useRef(false);
 
     useEffect(() => {
@@ -120,8 +138,10 @@ function Home() {
     }
 
     let shipLogsData = {};
+    let shipLogsTimestamps = [];
     let shipData = {};
     let shipIndices = [];
+    let allLogsLoaded = true;
     let i = 0;
     for (let j = 0; j < ships.length; j++) {
         const ship = ships[j];
@@ -129,36 +149,67 @@ function Home() {
             const log = ship.logs[k];
             shipData[i] = ship;
             shipLogsData[i] = log;
+            shipLogsTimestamps.push(log.timestamp);
             shipIndices.push(i);
             i++;
         }
     }
 
-    shipIndices.reverse();
-    const shipLogs = shipIndices.map((index) => {
-        const currentLog = shipLogsData[index];
-        const currentShip = shipData[index];
-        const randomId = Math.floor(Math.random() * 100);
-        return (
-            <motion.div style={{display: "inline-block"}} key={currentLog.id}
-            initial={{opacity: 0, y: 400}}
-            whileInView={{opacity: 1, y: 0, transition: {duration: 0.5}}}>
-                <Card
-                    style={{width: "300px", height: "400px", margin: "10px", display: "inline-block", verticalAlign: "top"}}>
-                    <img alt="(Ship Log Illustration)" src={"https://picsum.photos/id/" + randomId + "/300/200"}/>
-                    <CardBody>
-                        <CardTitle>{currentShip.name} - Ship Log</CardTitle>
-                        <CardSubtitle className="text-muted">{currentLog.timestamp}</CardSubtitle>
-                        <CardText>{currentLog.description}</CardText>
-                    </CardBody>
-                </Card>
-            </motion.div>
-        );
+    allLogsLoaded = i <= maxLogs;
+    shipIndices.sort(function(a, b) {
+        return Date.parse(shipLogsTimestamps[b]) - Date.parse(shipLogsTimestamps[a]);
     });
+    shipIndices.splice(maxLogs);
+    
+    let shipLogsCore = (
+        <Container style={{textAlign: "center"}}>
+            <p>No ship events have occurred so far.</p>
+            <Button onClick={() => fetchShips()}>Refresh</Button>
+        </Container>
+    );
+    if (shipIndices.length > 0) {
+        shipLogsCore = shipIndices.map((index) => {
+            const currentLog = shipLogsData[index];
+            const currentShip = shipData[index];
+            const randomId = Math.floor(Math.random() * 100);
+            return (
+                <motion.div style={{display: "inline-block"}} key={currentLog.id}
+                initial={{opacity: 0, y: 400}}
+                whileInView={{opacity: 1, y: 0, transition: {duration: 0.5}}}>
+                    <Card
+                        style={{width: "300px", height: "400px", margin: "10px", display: "inline-block", verticalAlign: "top"}}>
+                        <img alt="(Ship Log Illustration)" src={"https://picsum.photos/id/" + randomId + "/300/200"}/>
+                        <CardBody>
+                            <CardTitle>{currentShip.name} - Ship Log</CardTitle>
+                            <CardSubtitle className="text-muted">{currentLog.timestamp}</CardSubtitle>
+                            <CardText>{currentLog.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </motion.div>
+            );
+        });
+    }
+
+    let shipLogsButton = (
+        <motion.div initial={{opacity: 0}} whileInView={{opacity: 1, y: 0, transition: {duration: 1.0}}}>
+            <Container style={{textAlign: "center", marginTop: "10px", marginBottom: "10px"}}>
+                <Button onClick={() => showMoreShipLogs()}>Show More Ship Events</Button>
+            </Container>
+        </motion.div>
+    );
+
+    let shipLogs = (
+        <>
+            {shipLogsCore}
+            {shipIndices.length > 0 && !allLogsLoaded ? shipLogsButton : ""}
+        </>
+    );
 
     let missionEventsData = {};
+    let missionEventsTimestamps = [];
     let missionData = {};
     let missionIndices = [];
+    let allEventsLoaded = true;
     i = 0;
     for (let j = 0; j < missions.length; j++) {
         const mission = missions[j];
@@ -166,32 +217,61 @@ function Home() {
             const event = mission.events[k];
             missionData[i] = mission;
             missionEventsData[i] = event;
+            missionEventsTimestamps.push(event.timestamp);
             missionIndices.push(i);
             i++;
         }
     }
 
-    missionIndices.reverse();
-    const eventLogs = missionIndices.map((index) => {
-        const currentEvent = missionEventsData[index];
-        const currentMission = missionData[index];
-        const randomId = Math.floor(Math.random() * 100);
-        return (
-            <motion.div style={{display: "inline-block"}} key={currentEvent.id}
-            initial={{opacity: 0, y: 400}}
-            whileInView={{opacity: 1, y: 0, transition: {duration: 0.5}}}>
-                <Card
-                    style={{width: "300px", height: "400px", margin: "10px", display: "inline-block", verticalAlign: "top"}}>
-                    <img alt="(Mission Event Illustration)" src={"https://picsum.photos/id/" + randomId + "/300/200"}/>
-                    <CardBody>
-                        <CardTitle>{currentMission.title} - Ship Event</CardTitle>
-                        <CardSubtitle className="text-muted">{currentEvent.timestamp}</CardSubtitle>
-                        <CardText>{currentEvent.description}</CardText>
-                    </CardBody>
-                </Card>
-            </motion.div>
-        );
+    allEventsLoaded = i <= maxEvents;
+    missionIndices.sort(function(a, b) {
+        return Date.parse(missionEventsTimestamps[b]) - Date.parse(missionEventsTimestamps[a]);
     });
+    missionIndices.splice(maxEvents);
+
+    let missionEventsCore = (
+        <Container style={{textAlign: "center"}}>
+            <p>No mission events have occurred so far.</p>
+            <Button onClick={() => fetchMissions()}>Refresh</Button>
+        </Container>
+    );
+    if (missionIndices.length > 0) {
+        missionEventsCore = missionIndices.map((index) => {
+            const currentEvent = missionEventsData[index];
+            const currentMission = missionData[index];
+            const randomId = Math.floor(Math.random() * 100);
+            return (
+                <motion.div style={{display: "inline-block"}} key={currentEvent.id}
+                initial={{opacity: 0, y: 400}}
+                whileInView={{opacity: 1, y: 0, transition: {duration: 0.5}}}>
+                    <Card
+                        style={{width: "300px", height: "400px", margin: "10px", display: "inline-block", verticalAlign: "top"}}>
+                        <img alt="(Mission Event Illustration)" src={"https://picsum.photos/id/" + randomId + "/300/200"}/>
+                        <CardBody>
+                            <CardTitle>{currentMission.title} - Ship Event</CardTitle>
+                            <CardSubtitle className="text-muted">{currentEvent.timestamp}</CardSubtitle>
+                            <CardText>{currentEvent.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </motion.div>
+            );
+        });
+    }
+
+    let missionEventsButton = (
+        <motion.div initial={{opacity: 0}} whileInView={{opacity: 1, y: 0, transition: {duration: 1.0}}}>
+            <Container style={{textAlign: "center", marginTop: "10px", marginBottom: "10px"}}>
+                <Button onClick={() => showMoreMissionEvents()}>Show More Mission Events</Button>
+            </Container>
+        </motion.div>
+    );
+
+    let missionEvents = (
+        <>
+            {missionEventsCore}
+            {missionIndices.length > 0 && !allEventsLoaded ? missionEventsButton : ""}
+        </>
+    );
 
     return (
         <div>
@@ -200,6 +280,10 @@ function Home() {
             <p>Send ships to "space" to do things.</p>
 
             <HomeCarousel/>
+
+            <Container style={{marginTop: "10px", marginBottom: "10px"}}>
+                <Button onClick={() => refresh()}>Refresh</Button>
+            </Container>
 
             <h2>Available Ships</h2>
             {availableShipsBase}
@@ -211,8 +295,10 @@ function Home() {
             </Container>
             <h2>Mission Events</h2>
             <Container style={{textAlign: "left"}}>
-                {eventLogs}
+                {missionEvents}
             </Container>
+
+            <AppFooter/>
         </div>
     );
 }
